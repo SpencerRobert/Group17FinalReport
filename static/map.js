080d9295b97d445ee9d1e366cd1edfaf2f6379f6
spencerpoultney <spoultne@uwo.ca>
2023-03-16 00:56:47 -0400

    Added map script

---
+++ static/map.js
@@ -0,0 +1,35 @@
+let called = false
+document.addEventListener('scroll', e => {
+  if (document.documentElement.scrollTop >= 12000) {
+    if (called) return
+    called = true
+    calledEvent()
+  }
+})
+
+function calledEvent() {
+	
+	console.log("TRIGGERED");
+  
+  var s5 = document.getElementById("state5");
+  var s4 = document.getElementById("state4");
+  var s3 = document.getElementById("state3");
+  var s2 = document.getElementById("state2");
+  var s1 = document.getElementById("state1");
+  
+  s5.style.top = '';
+  s5.style.position = 'initial';
+  
+  s4.style.top = '';
+  s4.style.position = 'initial';
+  
+  s3.style.top = '';
+  s3.style.position = 'initial';
+  
+  s2.style.top = '';
+  s2.style.position = 'initial';
+  
+  s1.style.top = '';
+  s1.style.position = 'initial';
+  
+}
